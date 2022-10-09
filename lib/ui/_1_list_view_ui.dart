48f17efa602c3d689fa657f9fd35ff44a79fbc9e
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-10-09 16:04:23 -0500

    Uso de ListView

---
+++ lib/ui/_1_list_view_ui.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListViewUI01 extends StatelessWidget {
+  const ListViewUI01({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("LisViewUI01")),
+        body: ListView(
+          children: const [Text("Hola mundo!")],
+        ));
+  }
+}
