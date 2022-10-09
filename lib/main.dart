627b67ac00f0be8db06cc687b3bb86067fefa187
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-10-09 17:03:31 -0500

    Uso de ListView dinámico (P3)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:comps_app/ui/_4_list_view_ui.dart';
+import 'package:comps_app/ui/_5_list_view_ui.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Componentes',
-        home: ListViewUI04());
+        home: ListViewUI05());
   }
 }
---
+++ lib/ui/_5_list_view_ui.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ListViewUI05 extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Supers Smash',
+    'Final Fanstay'
+  ];
+  const ListViewUI05({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("LisViewUI04")),
+        //Aquí se elige un constructor especial (ListView.separated) del ListView para agregar
+        //un widget separador entre ítems.
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, index) => Text('Hola ${options[index]}!'),
+          separatorBuilder: (context, index) => const Divider(),
+        ));
+  }
+}
