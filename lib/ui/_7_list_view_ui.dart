f01fee81978ffaca9faba1b6ec1d7c4d8c1f734a
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-10-09 19:59:26 -0500

    Uso de ListView dinámico (P5): uso de onTap

---
+++ lib/ui/_7_list_view_ui.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ListViewUI07 extends StatelessWidget {
+  final options = const [
+    'Megaman',
+    'Metal Gear',
+    'Supers Smash',
+    'Final Fanstay'
+  ];
+  const ListViewUI07({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("LisViewUI04"),
+          elevation: 0,
+          backgroundColor: Colors.indigo,
+        ),
+        //Aquí se elige un constructor especial (ListView.separated) del ListView para agregar
+        //un widget separador entre ítems.
+        body: ListView.separated(
+          itemCount: options.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text('Hola! ${options[index]}!'),
+            trailing:
+                const Icon(Icons.arrow_right_outlined, color: Colors.indigo),
+            onTap: () {
+              final game = options[index];
+              print(game);
+            },
+          ),
+          separatorBuilder: (context, index) => const Divider(),
+        ));
+  }
+}
